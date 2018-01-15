0ba928f449936a596718c3ee687b1fc113fb8a1a
Lowie Huyghe <iam@lowiehuyghe.com>
2018-01-15 23:02:20 +0100

    #12: Added tests for console override and logger options.

--- lib/logging/loggingserviceprovider.spec.ts
+++ lib/logging/loggingserviceprovider.spec.ts
@@ -7,8 +7,10 @@ import {
   Logger,
   transports
 } from 'winston'
+import * as sinon from 'sinon'
 
 describe('loggingserviceprovider.ts', () => {
+  const consoleMethods = ['log', 'info', 'warn', 'error', 'debug']
 
   it('should have no logger without config', () => {
     const edmunds = new Edmunds()
@@ -50,12 +52,123 @@ describe('loggingserviceprovider.ts', () => {
     const edmunds = new Edmunds()
     edmunds.config = importFresh('config')
 
+    const originalConsole: any = {}
+    for (let method of consoleMethods) {
+      originalConsole[method] = (console as any)[method]
+    }
+
     expect(edmunds.logger).to.be.an('undefined')
     edmunds.register(LoggingServiceProvider)
     expect(edmunds.logger).to.be.instanceof(Logger)
 
+    expect(edmunds.logger.level).to.equal('info')
     expect(edmunds.logger.transports.console).to.not.be.a('undefined')
     expect(edmunds.logger.transports.console).to.be.instanceof(transports.Console)
+    for (let method of consoleMethods) {
+      expect(originalConsole[method]).to.equal((console as any)[method])
+    }
+  })
+
+  it('should have logger options', () => {
+    // Override config
+    process.env.NODE_CONFIG = JSON.stringify({
+      logging: {
+        enabled: true,
+        logger: {
+          handleExceptions: true,
+          level: 'warn',
+          exitOnError: false
+        },
+        instances: [{
+          name: 'console',
+          driver: 'Console'
+        }]
+      }
+    })
+    const edmunds = new Edmunds()
+    edmunds.config = importFresh('config')
+    edmunds.register(LoggingServiceProvider)
+
+    expect(edmunds.logger.level).to.equal('warn')
+  })
+
+  it('should not override console', () => {
+    // Override config
+    process.env.NODE_CONFIG = JSON.stringify({
+      logging: {
+        enabled: true,
+        overrideConsole: false,
+        instances: [{
+          name: 'console',
+          driver: 'Console'
+        }]
+      }
+    })
+    const edmunds = new Edmunds()
+    edmunds.config = importFresh('config')
+
+    const originalConsole: any = {}
+    for (let method of consoleMethods) {
+      originalConsole[method] = (console as any)[method]
+    }
+
+    expect(edmunds.logger).to.be.an('undefined')
+    edmunds.register(LoggingServiceProvider)
+    expect(edmunds.logger).to.be.instanceof(Logger)
+
+    expect(edmunds.logger.level).to.equal('info')
+    expect(edmunds.logger.transports.console).to.not.be.a('undefined')
+    expect(edmunds.logger.transports.console).to.be.instanceof(transports.Console)
+    for (let method of consoleMethods) {
+      expect(originalConsole[method]).to.equal((console as any)[method])
+    }
+  })
+
+  it('should override console', () => {
+    // Override config
+    process.env.NODE_CONFIG = JSON.stringify({
+      logging: {
+        enabled: true,
+        overrideConsole: true,
+        instances: [{
+          name: 'console',
+          driver: 'Console'
+        }]
+      }
+    })
+    const edmunds = new Edmunds()
+    edmunds.config = importFresh('config')
+
+    const originalConsole: any = {}
+    for (let method of consoleMethods) {
+      originalConsole[method] = (console as any)[method]
+    }
+
+    const overriddenConsole = { ...originalConsole }
+    try {
+      edmunds.register(LoggingServiceProvider)
+      for (let method of consoleMethods) {
+        overriddenConsole[method] = (console as any)[method]
+      }
+    } finally {
+      for (let method of consoleMethods) {
+        (console as any)[method] = originalConsole[method]
+      }
+    }
+
+    for (let method of consoleMethods) {
+      expect(overriddenConsole[method]).to.not.equal(originalConsole[method])
+    }
+
+    // Stub and check methods
+    const anyLogger: any = edmunds.logger
+    for (let method of consoleMethods) {
+      const loggerMethod = method === 'log' ? 'verbose' : method
+
+      anyLogger[loggerMethod] = sinon.stub()
+      overriddenConsole[method]('This is a message')
+      expect(anyLogger[loggerMethod].called, method).to.equal(true)
+    }
   })
 
 })
