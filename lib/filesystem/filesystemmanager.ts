2cd1486f08f33ac2bb348cee6af0534f39f63466
Lowie Huyghe <iam@lowiehuyghe.com>
2018-05-23 00:21:33 +0200

    #15: Added manager, service providers and drivers for filesystem

---
+++ lib/filesystem/filesystemmanager.ts
@@ -0,0 +1,38 @@
+import { Manager } from '../support/manager'
+import Local from './drivers/local'
+import GoogleCloudStorage from './drivers/googlecloudstorage'
+
+export default class FileSystemManager extends Manager {
+  /**
+   * Create local driver
+   * @param {any} config
+   * @returns {Local}
+   */
+  protected createLocal (config: any): Local {
+    const storagePath = config.path
+    const prefix = config.prefix
+
+    if (!storagePath) {
+      throw new Error(`'path'-config is missing for filesystem-instance '${config.name}'`)
+    }
+
+    return new Local(storagePath, prefix)
+  }
+
+  /**
+   * Create google cloud storage driver
+   * @param {any} config
+   * @returns {GoogleCloudStorage}
+   */
+  protected createGooglecloudstorage (config: any): GoogleCloudStorage {
+    const bucket = config.bucket
+    const storagePath = config.path
+    const prefix = config.prefix
+
+    if (!bucket || !storagePath) {
+      throw new Error(`'bucket'- or 'path'-config is missing for filesystem-instance '${config.name}'`)
+    }
+
+    return new GoogleCloudStorage(bucket, storagePath, prefix)
+  }
+}
