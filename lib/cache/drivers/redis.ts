8deea8ab3375d14a978de72f6d7c63190700c462
Lowie Huyghe <iam@lowiehuyghe.com>
2018-06-06 23:42:17 +0200

    Revert "#145: Moving every package into it's own place."
    This reverts commit 3cc6a81e36721e06cf9b8169ffe4f63820c47d35.

---
+++ lib/cache/drivers/redis.ts
@@ -0,0 +1,85 @@
+import CacheDriverInterface from '../../foundation/cachedriverinterface'
+import * as redis from 'redis'
+
+/**
+ * Caching class using Redis
+ */
+export default class Redis implements CacheDriverInterface {
+  /**
+   * Database reference
+   */
+  public redis: redis.RedisClient
+
+  /**
+   * Constructor
+   * @param {redis.ClientOpts} config
+   */
+  constructor (config: redis.ClientOpts) {
+    this.redis = redis.createClient(config)
+  }
+
+  /**
+   * Set value in the cache
+   * @param {string} key
+   * @param data
+   * @param {number} lifetime
+   * @return {Promise<void>}
+   */
+  set (key: string, data: any, lifetime: number): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.redis.set(key, data, 'EX', lifetime, (err: Error, response: 'OK' | undefined) => {
+        if (err) {
+          reject(err)
+        } else if (response !== 'OK') {
+          reject(new Error(`Could not set redis-value with key "${key}"`))
+        } else {
+          resolve()
+        }
+      })
+    })
+  }
+
+  /**
+   * Get the value from cache
+   * @param {string} key
+   * @return {Promise<any>}
+   */
+  async get (key: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.redis.get(key, (err: Error, response: string) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve(response === null ? undefined : response)
+        }
+      })
+    })
+  }
+
+  /**
+   * Delete the value from cache
+   * @param {string} key
+   * @return {Promise<void>}
+   */
+  del (key: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.redis.del(key, (err: Error, response: number) => {
+        if (err) {
+          reject(err)
+        } else if (response !== 1) {
+          reject(new Error(`Could not delete redis-value with key "${key}" (${response})`))
+        } else {
+          resolve()
+        }
+      })
+    })
+  }
+
+  /**
+   * Close the connection
+   * @returns {Promise<void>}
+   */
+  async close (): Promise<void> {
+    this.redis.end()
+  }
+}
---
+++ lib/filesystem/filesystemmanager.spec.ts
@@ -0,0 +1,67 @@
+import Edmunds from '../edmunds'
+import FileSystemManager from './filesystemmanager'
+import * as chai from 'chai'
+import * as appRootPath from 'app-root-path'
+import 'mocha'
+import Local from './drivers/local'
+import GoogleCloudStorage from './drivers/googlecloudstorage'
+
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const expect = chai.expect
+
+describe('FileSystemManager', () => {
+
+  it('should throw error when instantiating local without path', async () => {
+    const config = [{
+      name: 'local',
+      driver: 'local'
+    }]
+
+    const edmunds = new Edmunds(appRootPath.path)
+    const manager = new FileSystemManager(edmunds, config)
+
+    await expect(manager.get()).to.be.rejectedWith("'path'-config is missing for filesystem-instance 'local'")
+  })
+
+  it('should have local', async () => {
+    const config = [{
+      name: 'local',
+      driver: 'local',
+      path: 'storage'
+    }]
+
+    const edmunds = new Edmunds(appRootPath.path)
+    const manager = new FileSystemManager(edmunds, config)
+
+    expect(await manager.get()).to.be.an.instanceof(Local)
+    expect(await manager.get('local')).to.be.an.instanceof(Local)
+  })
+
+  it('should throw error when instantiating Google Cloud Storage without bucket', async () => {
+    const config = [{
+      name: 'googlecloudstorage',
+      driver: 'googlecloudstorage'
+    }]
+
+    const edmunds = new Edmunds(appRootPath.path)
+    const manager = new FileSystemManager(edmunds, config)
+
+    await expect(manager.get()).to.be.rejectedWith("'bucket'-config is missing for filesystem-instance 'googlecloudstorage'")
+  })
+
+  it('should have Google Cloud Storage', async () => {
+    const config = [{
+      name: 'googlecloudstorage',
+      driver: 'googlecloudstorage',
+      path: 'storage',
+      bucket: 'non-existing-bucket'
+    }]
+    const edmunds = new Edmunds(appRootPath.path)
+    const manager = new FileSystemManager(edmunds, config)
+
+    expect(await manager.get()).to.be.an.instanceof(GoogleCloudStorage)
+    expect(await manager.get('googlecloudstorage')).to.be.an.instanceof(GoogleCloudStorage)
+  })
+
+})
